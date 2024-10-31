9aa35d331a818210e701f08ae8c75df8dc0c941b
YonanPrasetyo <yonanprasetyo91792@gmail.com>
2024-10-31 19:28:56 +0700

    fix: fitur tambah stock

--- app/models/Stock.php
+++ app/models/Stock.php
@@ -9,9 +9,21 @@ class Stock extends Database {
     }
 
     public function tambah($ID_Produk, $Jumlah_Stock) {
-        $query = "INSERT INTO stockproduk (ID_Produk, Jumlah_Stock) 
-                  VALUES ('$ID_Produk', '$Jumlah_Stock')";
-        return $this->conn->exec($query);
+        $query = "SELECT * FROM stockproduk WHERE ID_Produk = '$ID_Produk'";
+        $stock = $this->conn->query($query);
+        if ($stock->rowCount() > 0) {
+            $stock = $stock->fetch(PDO::FETCH_ASSOC);
+            $Jumlah_Stock = $stock['Jumlah_Stock'] + $Jumlah_Stock;
+            $query = "UPDATE stockproduk
+                      SET Jumlah_Stock = '$Jumlah_Stock'
+                      WHERE ID_Produk = '$ID_Produk'";
+            return $this->conn->exec($query);
+        }else {
+            $query = "INSERT INTO stockproduk (ID_Produk, Jumlah_Stock) 
+                      VALUES ('$ID_Produk', '$Jumlah_Stock')";
+            return $this->conn->exec($query);
+        }
+
     }
 
     public function readAll() {
